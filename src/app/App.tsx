4f611b4f634bb424900a3455d072ec107576957e
Maxim Bil <maximushka@Maxims-MacBook-Pro.local>
2023-04-13 01:16:32 +0700

    Обновил структуру проекта и зарефакторил роуты

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import './styles/index.scss';
+import { Link } from 'react-router-dom';
+import { useTheme } from 'app/providers/ThemeProvider';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppRouter } from 'app/providers/router';
+
+const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>Change theme</button>
+            <Link to="/">Main</Link>
+            <Link to="/about">About</Link>
+            <AppRouter />
+        </div>
+    );
+};
+
+export default App;
